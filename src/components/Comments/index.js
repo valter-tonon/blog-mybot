1e9cb37399c3a732e22225b5eac53297a0dfacf6
valter-tonon <56795237+valter-tonon@users.noreply.github.com>
2020-05-23 18:20:31 -0300

    fixComments

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -11,7 +11,7 @@ const Comments = ({ url, title }) => {
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="Mybot"
+        shortname="CasaBella"
         identifier={completeURL}
         title={title}
         url={completeURL}
